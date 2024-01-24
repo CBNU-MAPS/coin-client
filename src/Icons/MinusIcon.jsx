90a2dd146db801d4fd088efcf1df908dba5d23f8
kiuuon <k41471804@gmail.com>
2024-01-24 18:41:33 +0900

    style: 아이폰 화면에 맞게 픽셀 값 및 아이콘 수정

---
+++ src/Icons/MinusIcon.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function MinusIcon() {
+  return (
+    <svg
+      width="11"
+      height="6"
+      viewBox="0 0 11 6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path d="M0.2 5.49594V0.615939H10.68V5.49594H0.2Z" fill="black" />
+    </svg>
+  );
+}
