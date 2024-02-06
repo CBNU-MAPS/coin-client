3b5ebbd27dd9770eb5bac2dcbc7c5897751dc763
KangJiUng <woong236406@gmail.com>
2024-02-06 19:24:16 +0900

    feat: 유저 이름 모달 구현

---
+++ src/pages/Bingo/UserSettingModal/UserNameModal/UserNameModal.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import NextIcon from '../../../../Icons/NextIcon';
+import style from './UserNameModal.module.scss';
+
+function UserNameModal({ userName, setUserName, setModalType }) {
+  const inputChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const nextButtonClick = () => {
+    if (userName === '') {
+      alert('이름을 입력해주세요.');
+    } else {
+      setModalType('userAvatarModal');
+    }
+  };
+
+  return (
+    <div className={`${style.modal}`}>
+      <div className={`${style.modalTitle} bold32`}>이름을 알려주세요.</div>
+      <input
+        className={`${style.modalInput} medium24`}
+        value={userName}
+        onChange={inputChange}
+      />
+      <div className={`${style.nextButton}`}>
+        <button
+          type="button"
+          aria-label="Next Button"
+          onClick={nextButtonClick}>
+          <NextIcon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+UserNameModal.propTypes = {
+  userName: PropTypes.string.isRequired,
+  setUserName: PropTypes.func.isRequired,
+  setModalType: PropTypes.func.isRequired,
+};
+
+export default UserNameModal;
