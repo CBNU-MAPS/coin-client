c3dab91dd4ed7f62e6619f0555f49642a683de04
KangJiUng <woong236406@gmail.com>
2024-02-06 19:21:33 +0900

    feat: avatarMappingObject.js 파일 생성

---
+++ src/utils/avatarMappingObject.js
@@ -0,0 +1,14 @@
+const avatarMappingObject = {
+  0: '🐥',
+  1: '🧚',
+  2: '👩‍🎤',
+  3: '👅',
+  4: '🤖',
+  5: '🐭',
+  6: '🐾',
+  7: '🔥',
+  8: '🐳',
+  9: '🥸',
+};
+
+export default avatarMappingObject;
