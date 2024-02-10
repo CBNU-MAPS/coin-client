c275851b32377139a7e923e22f0275e5e0c6f72c
kiuuon <k41471804@gmail.com>
2024-02-10 23:59:36 +0900

    feat: 웹 소켓 연결 후 방정보 받아오기

--- src/pages/Bingo/Bingo.jsx
+++ src/pages/Bingo/Bingo.jsx
@@ -4,34 +4,44 @@ import { useLocation } from 'react-router-dom';
 import { Client } from '@stomp/stompjs';
 
 import style from './Bingo.module.scss';
+import useBingoInfoStore from '../../stores/bingoInfoStore';
+import useQuestionStore from '../../stores/questionStore';
 
 function Bingo() {
   const client = useRef({});
   const location = useLocation();
 
+  const roomCode = location.pathname.split('/')[2];
+  const { setBingoName, setBingoHeadCount, setBingoSize } = useBingoInfoStore();
+  const { setQuestions } = useQuestionStore();
+
   useEffect(() => {
     const subscribe = () => {
-      client.current.subscribe(`/room`, (data) => {
-        // eslint-disable-next-line no-console
-        console.log(data);
+      client.current.subscribe(`/room/${roomCode}`, (data) => {
+        const { bingoName, bingoSize, bingoHeadCount, questions } = JSON.parse(
+          data.body,
+        );
+        setBingoName(bingoName);
+        setBingoHeadCount(bingoHeadCount);
+        setBingoSize(bingoSize);
+        setQuestions(questions);
       });
     };
 
     client.current = new Client({
       brokerURL: import.meta.env.VITE_BROKER_URL,
+      connectHeaders: {
+        code: roomCode,
+      },
       onConnect: () => {
         subscribe();
-        client.current.publish({
-          destination: '/room',
-          body: 'Hello, STOMP',
-        });
       },
     });
 
     client.current.activate();
 
     return () => client.current.deactivate();
-  }, [location]);
+  }, [roomCode, setBingoHeadCount, setBingoName, setBingoSize, setQuestions]);
 
   return <div className={`${style.test} bold26`}>빙고페이지 입니다.</div>;
 }
