f1d4ce010bf3ac2f3e2df17badc968b5d631883f
KangJiUng <woong236406@gmail.com>
2024-02-14 21:25:24 +0900

    feat: 링크 복사 버튼 UI 구현

--- src/pages/Bingo/BingoHeader/BingoHeader.jsx
+++ src/pages/Bingo/BingoHeader/BingoHeader.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import useBingoInfoStore from '../../../stores/bingoInfoStore';
 
 import style from './BingoHeader.module.scss';
+import useBingoInfoStore from '../../../stores/bingoInfoStore';
+import LinkCopyIcon from '../../../Icons/LinkCopyIcon';
 
 function BingoHeader() {
   const { bingoName } = useBingoInfoStore();
+
   return (
     <div className={style.header}>
       <div className={`${style.bingoName} bold32`}>{bingoName}</div>
+      <button
+        type="button"
+        aria-label="Link Copy Button"
+        className={`${style.copybutton} bold18`}>
+        <LinkCopyIcon />
+        URL
+      </button>
     </div>
   );
 }
