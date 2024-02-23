40cde18f3b50c78c0b38b28f149e8ac3cc149ba4
Kiwon Kim <kiuuon29@gmail.com>
2024-02-23 15:56:49 +0900

    feat: 질문 입력 모달 구현 (#40)
    * feat: 질문 모달 overlay 생성
    
    * feat: anserStore 생성
    
    * feat: input 타입의 질문을 입력하는 모달 구현
    
    * feat: select 타입의 질문을 입력하는 모달 구현
    
    * feat: 모달 밖 클릭 시 모달창 닫기
    
    * style: 빙고판과 select 모달 css 수정

--- src/pages/Bingo/BingoBoard/BingoBoard.jsx
+++ src/pages/Bingo/BingoBoard/BingoBoard.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import style from './BingoBoard.module.scss';
 import useBingoInfoStore from '../../../stores/bingoInfoStore';
 import useQuestionStore from '../../../stores/questionStore';
+import QuestionModalOverlay from './QuestionModal/QuestionModalOverlay';
+import useAnswerStore from '../../../stores/answerStore';
 
 function BingoBoard() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedQuestionId, setSelectedQuestionId] = useState(null);
   const { bingoSize } = useBingoInfoStore();
   const { questions } = useQuestionStore();
+  const { answers } = useAnswerStore();
 
   const bingoCellSize = {
-    width: `${21 * (bingoSize + 2 * (4 - bingoSize))}px`,
-    height: `${21 * (bingoSize + 2 * (4 - bingoSize))}px`,
+    width: `${21 * (bingoSize + 2 * (4 - bingoSize)) - 15}px`,
+    height: `${21 * (bingoSize + 2 * (4 - bingoSize)) - 15}px`,
+  };
+
+  const handleCellClick = (id) => {
+    setSelectedQuestionId(id);
+    setIsModalOpen(true);
   };
 
   return (
     <div className={`${style.bingoBoard}`}>
       {questions.map((question) => {
+        const answer =
+          answers?.filter((item) => item.id === question.id)[0]?.answer || '';
+
         return (
           <div
             key={question.id}
             className={`${style.bingoCell} bold18`}
-            style={bingoCellSize}>
-            {null}
+            style={bingoCellSize}
+            onClick={() => handleCellClick(question.id)}
+            onKeyDown={() => handleCellClick(question.id)}
+            role="presentation">
+            {answer}
           </div>
         );
       })}
+      {isModalOpen && (
+        <QuestionModalOverlay
+          selectedQuestionId={selectedQuestionId}
+          setIsModalOpen={setIsModalOpen}
+        />
+      )}
     </div>
   );
 }
